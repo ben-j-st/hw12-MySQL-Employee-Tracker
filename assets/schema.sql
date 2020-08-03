0fc6fa1b101f4fa8410c71102cf172216ff0e6ee
Ben Stephens <ben_j_stephens@hotmail.com>
2020-08-03 22:11:03 +1000

    fixed error with reference in schema

--- assets/schema.sql
+++ assets/schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE employee (
     manager_id int,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES (id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 
